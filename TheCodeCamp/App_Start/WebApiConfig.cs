941acaef4f9d553d1a57c421bbd8fac1bc5fcfdb
seoninja13 <dachevivo@gmail.com>
2020-03-25 15:29:40 -0700

    Add project files.

---
+++ TheCodeCamp/App_Start/WebApiConfig.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Web.Http;
+using Microsoft.Web.Http.Versioning;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace TheCodeCamp
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            AutofacConfig.Register();
+
+            config.AddApiVersioning(cfg =>
+           {
+               cfg.DefaultApiVersion = new ApiVersion(1, 1);
+               cfg.AssumeDefaultVersionWhenUnspecified = true;
+               cfg.ReportApiVersions = true;
+               cfg.ApiVersionReader = ApiVersionReader.Combine(
+                   new HeaderApiVersionReader("X-Version"),
+                   new QueryStringApiVersionReader("ver"));
+               
+           });
+
+            // Change Case of JSON
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver =
+              new CamelCasePropertyNamesContractResolver();
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
+        }
+    }
+}
---
+++ TheCodeCamp/Controllers/OperationsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace TheCodeCamp.Controllers
+{
+    public class OperationsController : ApiController
+    {
+        [HttpOptions]
+        [Route("api/refreshsettings")]
+        public IHttpActionResult RefreshAppSettings()
+        {
+            try
+            {
+                ConfigurationManager.RefreshSection("AppSettings");
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+
+                return InternalServerError(ex);
+            }
+
+
+        }
+
+    }
+}
